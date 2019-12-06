ff4d59908156b09ecde861ff85b68e2533bfc65a
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2019-12-06 14:03:01 +0530

    added the seconds to the question
    What changes you have made?
    .....
    
    Why this change is necessary?
    ....
    
    What is the impact of the change?
    .....

--- src/performAction.js
+++ src/performAction.js
@@ -4,6 +4,7 @@ stdin.setRawMode(true);
 stdin.setEncoding("utf8");
 
 const printQuestion = function(count, questionAnswer) {
+  console.clear();
   stdout.write(
     `\n${count + 1}=>  ${questionAnswer.question}\n${questionAnswer.options}\n`
   );
@@ -18,6 +19,7 @@ const getUserData = function(questionAndAnswers) {
   let score = 0;
   const processNextQuestion = function(data) {
     clearTimeout(timer);
+    clearInterval(timerForEachQuestion);
     let userAnswer = data.trim();
     if (isCorrectAnswer(questionAndAnswers[count], userAnswer)) {
       score = score + 1;
@@ -26,14 +28,26 @@ const getUserData = function(questionAndAnswers) {
     if (count < questionAndAnswers.length) {
       printQuestion(count, questionAndAnswers[count]);
       timer = setTimeout(() => processNextQuestion(""), 10000);
+      timerForEachQuestion = setInterval(timerForEachSecond(), 1000);
     } else {
+      stdout.clearLine();
       stdout.write(`score:${score}\n`);
       process.exit();
     }
   };
   printQuestion(0, questionAndAnswers[0]);
+  let timerForEachQuestion = setInterval(timerForEachSecond(), 1000);
   stdin.on("data", processNextQuestion);
   let timer = setTimeout(() => processNextQuestion(""), 10000);
+};
+
+const timerForEachSecond = function() {
+  let timeCount = 10;
+  return function() {
+    stdout.cursorTo(15, 13);
+    stdout.clearLine();
+    stdout.write(`seconds Remaning : ${--timeCount}`);
+  };
 };
 
 process.on("exit", () => stdout.write("Game Over"));
